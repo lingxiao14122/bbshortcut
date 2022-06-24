250777f5cba015d4f0410b43ec340bd5726fa1e9
ling xiao <lingxiao3218@gmail.com>
2022-06-24 19:53:07 +0800

    feat: download video function

--- c_script.js
+++ c_script.js
@@ -1,6 +1,14 @@
 document.addEventListener('keydown', (event) => {
-    console.log(event)
     const keychar = event.key
+    if (keychar === ">" && event.shiftKey) {
+        console.log("shit forward")
+    }
+    if (keychar === "<" && event.shiftKey) {
+        console.log("shit backward")
+    }
+    if (!(event.shiftKey || !event.ctrlKey || event.altKey)) {
+        return
+    }
     if (keychar === "m") {
         console.log("shit mute")
     }
@@ -17,12 +25,46 @@ document.addEventListener('keydown', (event) => {
         console.log("shit open close chat")
     }
     if (keychar === "d") {
-        console.log("shit download")
-    }
-    if (keychar === ">" && event.shiftKey) {
-        console.log("shit forward")
-    }
-    if (keychar === "<" && event.shiftKey) {
-        console.log("shit backward")
+        const video = document.getElementsByClassName('vjs-tech')[0]
+        console.log(video.src)
+        console.log("new request")
+        let xhr = new XMLHttpRequest();
+        function createCORSRequest(method, url) {
+            var xhr = new XMLHttpRequest();
+            if ("withCredentials" in xhr) {
+                xhr.open(method, url, true);
+            } else if (typeof XDomainRequest != "undefined") {
+                xhr = new XDomainRequest();
+                xhr.open(method, url);
+            } else {
+                xhr = null;
+            }
+            return xhr;
+        }
+        xhr = createCORSRequest('GET', video.src);
+        xhr.responseType = 'blob';
+        xhr.onload = function () {
+            console.log("onload")
+            let urlCreator = window.URL || window.webkitURL;
+            let videoUrl = urlCreator.createObjectURL(this.response);
+            let tag = document.createElement('a');
+            tag.href = videoUrl;
+            tag.target = '_blank';
+            tag.download = "recoding.mp4"
+            document.body.appendChild(tag);
+            tag.click();
+            document.body.removeChild(tag);
+        };
+        xhr.onprogress = function (pe) {
+            if (pe.lengthComputable) {
+                progressBar.max = pe.total
+                progressBar.value = pe.loaded
+            }
+            console.log("progress")
+        }
+        xhr.onerror = (err) => {
+            console.log("download fail", err)
+        };
+        xhr.send();
     }
 })
