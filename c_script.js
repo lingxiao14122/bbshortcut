1ac1d80b77538000b7f623f9c74d0b1f8b492911
weiliang79 <ling2010@hotmail.my>
2022-06-27 12:24:46 +0800

    feat: captions, fullscreen, side panel click

--- c_script.js
+++ c_script.js
@@ -1,20 +1,36 @@
+let sidePanel = false, fullscreen = false;  //false = side panel close, true = side panel open
+
 document.addEventListener('keydown', (event) => {
     console.log(event)
     const keychar = event.key
-    if (keychar === "m") {
-        console.log("shit mute")
-    }
     if (keychar === "c") {
-        console.log("shit caption")
-    }
-    if (keychar === "f") {
-        console.log("shit fullscreen")
-    }
-    if (keychar === "t") {
-        const elem = document.querySelector('button#side-panel-open')
+        const elem = document.querySelector('button#no-captions-dropdown-toggle')
         console.log(elem)
         elem.click()
-        console.log("shit open close chat")
+    }
+    // bug: fullscreen are functional but cannot exit fullscreen
+    if (keychar === "f") {
+        if(fullscreen === false){
+            const elem = document.querySelector('button#playback-enter-fullscreen')
+            console.log(elem)
+            elem.click()
+        } else {
+            const elem = document.querySelector('button#playback-exit-fullscreen')
+            console.log(elem)
+            elem.click()
+        }
+    }
+    if (keychar === "t") {
+        // bug: side panel can open but cannot close
+        if(sidePanel === false){
+            const elem = document.querySelector('button#side-panel-open')
+            console.log(elem)
+            elem.click()
+        } else {
+            const elem = document.querySelector('button#side-panel-close')
+            console.log(elem)
+            elem.click()
+        }
     }
     if (keychar === "d") {
         console.log("shit download")
