1d9f2951fc4a13e2c5ef4222ce81ea784def1c48
ling xiao <lingxiao3218@gmail.com>
2022-07-02 19:22:03 +0800

    fix: bug for side panel toggle + download

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let sidePanel = false, fullscreen = false;  //false = side panel close, true = side panel open
+let isSidePanelOpen = false;
 let speed = 2; // 2 is 1.0x speed
 
 const reduceSpeedCount = () => {
@@ -15,6 +15,7 @@ const increaseSpeedCount = () => {
 
 document.addEventListener('keydown', (event) => {
     const keychar = event.key
+    console.log(event)
 
     if (keychar === ">" && event.shiftKey) {
         const button = document.querySelector('button#speed-dropdown-toggle')
@@ -35,80 +36,30 @@ document.addEventListener('keydown', (event) => {
     if (!(event.shiftKey || !event.ctrlKey || event.altKey)) {
         return
     }
-
-    if (keychar === "m") {
-        console.log("shit mute")
-    }
     if (keychar === "c") {
         const elem = document.querySelector('button#no-captions-dropdown-toggle')
         console.log(elem)
         elem.click()
     }
-    // bug: fullscreen are functional but cannot exit fullscreen
     if (keychar === "f") {
-        if (fullscreen === false) {
-            const elem = document.querySelector('button#playback-enter-fullscreen')
-            console.log(elem)
-            elem.click()
-        } else {
-            const elem = document.querySelector('button#playback-exit-fullscreen')
-            console.log(elem)
-            elem.click()
+        if (document.querySelector('button#playback-enter-fullscreen')) {
+            document.querySelector('button#playback-enter-fullscreen').click()
+
+        } 
+        if (document.querySelector('button#playback-exit-fullscreen')) {
+            document.querySelector('button#playback-exit-fullscreen').click()
         }
     }
-    if (keychar === "t") {
-        // bug: side panel can open but cannot close
-        if (sidePanel === false) {
-            const elem = document.querySelector('button#side-panel-open')
-            console.log(elem)
-            elem.click()
+    if (keychar === "g") {
+        if (isSidePanelOpen) {
+            document.querySelector('button#side-panel-close').click()
+            isSidePanelOpen = false
         } else {
-            const elem = document.querySelector('button#side-panel-close')
-            console.log(elem)
-            elem.click()
+            document.querySelector('button#side-panel-open').click()
+            isSidePanelOpen = true
         }
     }
     if (keychar === "d") {
-        const video = document.getElementsByClassName('vjs-tech')[0]
-        console.log(video.src)
-        console.log("new request")
-        let xhr = new XMLHttpRequest();
-        function createCORSRequest(method, url) {
-            var xhr = new XMLHttpRequest();
-            if ("withCredentials" in xhr) {
-                xhr.open(method, url, true);
-            } else if (typeof XDomainRequest != "undefined") {
-                xhr = new XDomainRequest();
-                xhr.open(method, url);
-            } else {
-                xhr = null;
-            }
-            return xhr;
-        }
-        xhr = createCORSRequest('GET', video.src);
-        xhr.responseType = 'blob';
-        xhr.onload = function () {
-            console.log("onload")
-            let urlCreator = window.URL || window.webkitURL;
-            let videoUrl = urlCreator.createObjectURL(this.response);
-            let tag = document.createElement('a');
-            tag.href = videoUrl;
-            tag.target = '_blank';
-            tag.download = "recoding.mp4"
-            document.body.appendChild(tag);
-            tag.click();
-            document.body.removeChild(tag);
-        };
-        xhr.onprogress = function (pe) {
-            if (pe.lengthComputable) {
-                progressBar.max = pe.total
-                progressBar.value = pe.loaded
-            }
-            console.log("progress")
-        }
-        xhr.onerror = (err) => {
-            console.log("download fail", err)
-        };
-        xhr.send();
+        document.querySelector('button#download-recording').click()
     }
 })
